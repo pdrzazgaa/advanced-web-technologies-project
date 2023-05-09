e3d42d122a0c78a9766d1e285c4192f4e467c914
Paulina Drzazga <paulina@halpress.team>
2023-05-09 09:22:09 +0200

    Init spring project

---
+++ SpringRest-Backend/src/main/java/com/listek/springrestbackend/SpringRestBackendApplication.java
@@ -0,0 +1,13 @@
+package com.listek.springrestbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRestBackendApplication.class, args);
+    }
+
+}
