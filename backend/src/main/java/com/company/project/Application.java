c6860a3508da8ccde0ea16684b27780e4372f023
Paulina Drzazga <paulina@halpress.team>
2023-05-28 21:49:02 +0200

    Some changes

--- backend/src/main/java/com/company/project/Application.java
+++ backend/src/main/java/com/company/project/Application.java
@@ -4,9 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-//@EntityScan("com.company.project.models")
-//@ComponentScan(basePackages = { "com.company.project.*" })
-//@EnableJpaRepositories("com.company.project.repositories")
 public class Application {
 
     public static void main(String[] args) {
