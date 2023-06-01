08fba36738d42fda7f0fdec7850d4f22b5899a3b
Maria Markowiak <maria.markowiak@gmail.com>
2023-06-01 10:05:32 +0200

    Add env file

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,18 @@
 import React, { FC } from "react";
+import axios from "axios";
 
 const App: FC = () => {
+  axios
+    .get("http:localhost:8000/places")
+    .then(response => {
+      // Handle the successful response here
+      console.log(response.data);
+    })
+    .catch(error => {
+      // Handle errors here
+      console.error(error);
+    });
+
   return <div>front działa</div>;
 };
 
