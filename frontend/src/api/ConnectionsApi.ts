564468d14285120f7166a19c4ad066f117aaecf4
Maria Markowiak <maria.markowiak@gmail.com>
2023-06-19 18:50:14 +0200

    fix urls

--- frontend/src/api/ConnectionsApi.ts
+++ frontend/src/api/ConnectionsApi.ts
@@ -3,11 +3,11 @@ import { SearchParams } from "../types/SearchParams";
 import axios from "axios";
 
 const Api = axios.create({
-  baseURL: process.env.REACT_APP_,
+  baseURL: process.env.REACT_APP_API_URL as string,
   timeout: 10000,
 });
 
-const URL = `${process.env.REACT_APP_API_URL}/connection`;
+const URL = "/connection";
 export class ConnectionsApi {
   static async getAll(params: SearchParams) {
     const { data: places } = await Api.get<Connection[]>(URL, { params: params });
